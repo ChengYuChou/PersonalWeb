fddf6df039798756df338aabbea85ca87fdfe8bf
ChengYuChou <136459571+ChengYuChou@users.noreply.github.com>
2024-07-11 17:45:24 +0800

    Add files via upload

---
+++ 小專案/PersonalWeb/簡易社群平台.js
@@ -0,0 +1,7 @@
+const clickableDiv = document.getElementById('myClickableDiv');
+
+clickableDiv.addEventListener('click', function() {
+    window.location.href = 'file:///Users/zhouchengyu/Desktop/Yusweb/小專案/註冊頁面.html';
+});
+
+
